4c96d391a7a718eb25e7338c23b4984d2fee8013
Qiyu Chen <q5chen@ucsd.edu>
2023-12-11 20:44:06 -0500

    update projection display website to enable a simple demo

--- projection_website/display.py
+++ projection_website/display.py
@@ -5,6 +5,10 @@ from flask import render_template, Response, stream_with_context
 from flask import request
 from termcolor import colored
 
+import numpy as np
+
+from dog_config import dog_config
+
 app = Flask(__name__)
 
 ## Global var
@@ -13,30 +17,42 @@ debug_message = lambda x: app.logger.debug(colored("-- "+str(x), "red"))
 # DEBUG = True
 NEW_PARAM_FLAG = False # update when new params are received
 lock = threading.Lock()
+# homography = np.array([[ 3.91385031e-01, -7.76022044e-02, -1.84052185e+01],
+#        [-1.63380904e-16,  4.11629084e-01, -5.39234100e+01],
+#        [-1.84209407e-18, -1.00095597e-03,  1.00000000e+00]]) # iPad 480x640 reso px to table mm with origin at middle of osmo
+homography = np.array([[ 4.04417623e-01, -1.00672980e-16, -9.70602294e+01],
+       [-4.17131426e-17,  4.44550669e-01, -5.73470363e+01],
+       [-2.35018606e-18, -9.56022945e-04,  1.00000000e+00]]) # iPad 480x640 reso px to table mm with origin at middle of osmo
 
 ## display-related variables from the app
 GLOBAL_VARS = {
     "width":4096,
     "height":2304,
+    "coord_unit":"px",
+    "proj_half_width_mm": 280,
+    "table_mm_2_proj_px_scale":2.43,
 }
 display_vars = {
     "stage":1,
-    "scale":0.67,
-    "x_coord":20,
-    "y_coord":10,
+    "scale":0.71,
+    "x_coord":500,
+    "y_coord":100,
+    
+    "valid_stage":1, # stage excluding 0
 }
 display_vars_type = {
     "stage":int,
     "scale":float,
     "x_coord":float,
     "y_coord":float,
+    "valid_stage":int,
 }
 
 
 ## backend
 @app.route("/")
 def main():
-    return render_template('main.html.jinja', **display_vars, **GLOBAL_VARS)
+    return render_template('main.html.jinja', **display_vars, **GLOBAL_VARS, **dog_config)
     # return render_template('main.html.jinja', stage=stage, scale=scale, x_coord=x_coord, y_coord=y_coord)
 #     return (\
 # f"""
@@ -85,6 +101,11 @@ def set_param():
             display_vars[key] = display_vars_type[key](val)
         else:
             debug_message(f"Invalid parameter received: {key}")
+    
+    if "x_coord" in request.args and "y_coord" in request.args: # and display_vars["stage"]!=0
+        iPad_px_2_table_mm()
+    if display_vars["stage"] != 0:
+        display_vars["valid_stage"] = display_vars["stage"]
     # if DEBUG: print(request.args)
     debug_message(request.args)
     with lock:
@@ -93,12 +114,15 @@ def set_param():
 
 @app.route('/param/stream')
 def stream():
+    """
+    URL to listen to Server-Sent Events (SSE)
+    """
     def event_stream():
         global NEW_PARAM_FLAG
         if request.headers.get('accept') == 'text/event-stream':
             while True:
                 # data sending logic here
-                time.sleep(0.5) # check every 0.5 seconds
+                time.sleep(0.3) # check every 0.2 seconds
                 with lock:
                     if NEW_PARAM_FLAG:
                         NEW_PARAM_FLAG = False
@@ -125,3 +149,25 @@ def add_header(response):
     response.headers["Expires"] = "0"
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
+
+
+
+########## Geomotry-based backend coordinate conversion & re-localization
+def iPad_px_2_table_mm():
+    x = display_vars["x_coord"]
+    y = display_vars["y_coord"]
+    print("Received x,y:",x,y)
+    homo_coord = np.array([x, y, 1], dtype=np.float32)
+    table_mm_coord = homography @ homo_coord
+    table_mm_coord = table_mm_coord[:2] / table_mm_coord[2]
+    print("Homography fixed x,y:", table_mm_coord)
+
+    curr_stage = display_vars["stage"] if display_vars["stage"] else display_vars["valid_stage"]
+    current_step_size = dog_config["instruction_image_size"][ curr_stage ]
+    current_step_offset = dog_config["instruction_image_arrow_offset"][ curr_stage ]
+    display_vars["x_coord"] = (table_mm_coord[0]+GLOBAL_VARS["proj_half_width_mm"]) * GLOBAL_VARS["table_mm_2_proj_px_scale"]
+    display_vars["x_coord"] += -(1-display_vars["scale"])/2 * current_step_size[0] + current_step_offset[0]*display_vars["scale"]
+    display_vars["y_coord"] = table_mm_coord[1] * GLOBAL_VARS["table_mm_2_proj_px_scale"]
+    display_vars["y_coord"] += -(1-display_vars["scale"])/2 * current_step_size[1] + current_step_offset[1]*display_vars["scale"]
+
+    print("final x,y:", display_vars["x_coord"],display_vars["y_coord"])
---
+++ projection_website/dog_config.py
@@ -0,0 +1,26 @@
+dog_config = {
+    "instruction_image_folder": "",
+    "total_step_num": 8,
+    "instruction_gif_folder": "",
+    "final_step_image_path":"static/dog_imgs/finished.png",
+    "instruction_image_size":{
+        1: (687,687),
+        2: (693,347),
+        3: (693,431),
+        4: (703,396),
+        5: (238,269),
+        6: (568,319),
+        7: (323,320),
+        8: (250,321)
+    },
+    "instruction_image_arrow_offset":{
+        1: (0,0),
+        2: (0,0),
+        3: (0,0),
+        4: (0,-167),
+        5: (0,-41),
+        6: (0,0),
+        7: (-36,0),
+        8: (0,0)
+    },
+}
